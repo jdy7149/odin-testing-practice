4d9ffc274e3ba328bd5fca4390cd58d820fa6ebd
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-11-23 13:22:03 +0900

    End calculator.test.js

--- src/calculator.js
+++ src/calculator.js
@@ -7,7 +7,7 @@ const calculator = Object.freeze({
   },
   divide(a, b) {
     if (b === 0) {
-      throw Error('Cannot divide by 0.');
+      throw Error('Cannot divide by 0');
     }
 
     return a / b;
